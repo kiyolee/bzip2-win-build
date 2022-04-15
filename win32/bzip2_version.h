33be20ea4648fac183d697302ca6b620df524719
Kelvin Lee <kiyolee@gmail.com>
2022-04-16 08:57:12 +1000

    Fix dll version to 1.0.8

--- win32/bzip2_version.h
+++ win32/bzip2_version.h
@@ -1,4 +1,4 @@
-#define BZIP2_VERSION_STR   "1.0.7"
+#define BZIP2_VERSION_STR   "1.0.8"
 #define BZIP2_VER_MAJOR     1
 #define BZIP2_VER_MINOR     0
-#define BZIP2_VER_REVISION  7
+#define BZIP2_VER_REVISION  8
